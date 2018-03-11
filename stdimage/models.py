6b54b63d8cc41d0a6bb0965ae4713accbe70f7df
Tim O'Meara <tim.omeara@gmail.com>
2018-03-11 17:32:59 -0600

    add quality var

--- stdimage/models.py
+++ stdimage/models.py
@@ -68,11 +68,14 @@ class StdImageFieldFile(ImageFieldFile):
                 return variation_name
 
         resample = variation['resample']
+        if variation['quality']:
+            quality = variation['quality']
+        else:
+            quality = 80
 
         ImageFile.LOAD_TRUNCATED_IMAGES = True
         with storage.open(file_name) as f:
             with Image.open(f) as img:
-                save_kargs = {}
                 file_format = img.format
 
                 if cls.is_smaller(img, variation):
@@ -96,10 +99,6 @@ class StdImageFieldFile(ImageFieldFile):
                     if file_format == 'JPEG':
                         # http://stackoverflow.com/a/21669827
                         img = img.convert('RGB')
-                        save_kargs['optimize'] = True
-                        save_kargs['quality'] = 'web_high'
-                        if size[0] * size[1] > 10000:  # roughly <10kb
-                            save_kargs['progressive'] = True
 
                     if variation['crop']:
                         img = ImageOps.fit(
@@ -114,7 +113,8 @@ class StdImageFieldFile(ImageFieldFile):
                         )
 
                 with BytesIO() as file_buffer:
-                    img.save(file_buffer, file_format, **save_kargs)
+                    #img.save(file_buffer, file_format, **save_kargs)
+                    img.save(file_buffer, file_format, quality=quality, optimize=True, progressive=True)
                     f = ContentFile(file_buffer.getvalue())
                     storage.save(variation_name, f)
         return variation_name
